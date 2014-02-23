15547df24b0da74ca5149075de90af521d5ce084
cs76 <mailcs76@gmail.com>
2014-02-23 22:16:23 +0000

    port changed

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ app.configure(function() {
     app.use(express.static(path.join(__dirname,'public')));
 });
 
-var server = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 5000);
+
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
